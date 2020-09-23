8d099ae506d6631c1a8bddf6112b428681a68e3b
manasa-cm27 <manasa7n93@gmail.com>
2020-09-23 15:59:23 +0530

    first push

---
+++ first.py
@@ -0,0 +1,13 @@
+eid=int(input("Enter the eid:"))
+ename=input("Enter the ename:")
+esal=float(input("Enter the esal:"))
+
+print("Emp id:",eid)
+print("Emp name:",ename)
+print("Emp sal:",esal)
+
+print(eid,ename,esal)
+
+print("Emp id=%d , Emp name=%s , Emp sal=%g"%(eid,ename,esal))
+
+print("Emp id={0} Emp name={1} Emp sal={2}".format(eid,ename,esal))
